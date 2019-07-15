8c97504bd961a885704ee8fa9189bd51a30b05d1
hzwicker <hickzwicker@gmail.com>
2019-07-15 14:22:39 -0300

    Initial

---
+++ Data_WareHouse_Roupas_ETL_1/Tabelas/Dimensao_Cliente.sql
@@ -0,0 +1,14 @@
+﻿CREATE TABLE [dbo].[Dimensao_Cliente]
+(
+	[Id_Cliente] NVARCHAR(50) NOT NULL PRIMARY KEY, 
+    [Desc_Cliente] NVARCHAR(200) NULL, 
+    [Id_Cidade] NVARCHAR(50) NULL, 
+    [Desc_Cidade] NVARCHAR(200) NULL,
+	[Id_Estado] NVARCHAR(50) NULL, 
+    [Desc_Estado] NVARCHAR(200) NULL,
+	[Id_Regiao] NVARCHAR(50) NULL, 
+    [Desc_Regiao] NVARCHAR(200) NULL,
+	[Id_Segmento] NVARCHAR(50) NULL, 
+    [Desc_Segmento] NVARCHAR(200) NULL
+)
+
---
+++ Data_WareHouse_Roupas_ETL_1/Tabelas/Dimensao_Fabrica.sql
@@ -0,0 +1,5 @@
+﻿CREATE TABLE [dbo].[Dimensao_Fabrica]
+(
+	[Id_Fabrica] NVARCHAR(50) NOT NULL PRIMARY KEY, 
+    [Desc_Fabrica] NCHAR(200) NULL
+)
